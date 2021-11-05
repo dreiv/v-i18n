53cac7f7a3e6d7b79f5a0376b3a44b5844555899
Andrei Voicu <dreivoicu@gmail.com>
2021-11-05 12:58:12 +0200

    trial

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,31 +1,42 @@
-import { createI18n, LocaleMessages, VueMessageType } from "vue-i18n";
+import { nextTick } from "vue";
+import { createI18n } from "vue-i18n";
+import en from "@/locales/en.json";
 
-/**
- * Load locale messages
- *
- * The loaded `JSON` locale messages is pre-compiled by `@intlify/vue-i18n-loader`, which is integrated into `vue-cli-plugin-i18n`.
- * See: https://github.com/intlify/vue-i18n-loader#rocket-i18n-resource-pre-compilation
- */
-function loadLocaleMessages(): LocaleMessages<VueMessageType> {
-  const locales = require.context(
-    "./locales",
-    true,
-    /[A-Za-z0-9-_,\s]+\.json$/i
-  );
-  const messages: LocaleMessages<VueMessageType> = {};
-  locales.keys().forEach((key) => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
-    if (matched && matched.length > 1) {
-      const locale = matched[1];
-      messages[locale] = locales(key).default;
-    }
-  });
-  return messages;
+export const SUPPORT_LOCALES = ["en", "de", "ro", "zu"];
+
+export function setupI18n(options = { locale: "en", messages: { en } }) {
+  const i18n = createI18n(options);
+  setI18nLanguage(i18n, options.locale);
+  return i18n;
 }
 
-export default createI18n({
-  legacy: false,
-  locale: "en",
-  fallbackLocale: "en",
-  messages: loadLocaleMessages(),
-});
+export function setI18nLanguage(i18n: any, locale: any) {
+  if (i18n.mode === "legacy") {
+    i18n.global.locale = locale;
+  } else {
+    i18n.global.locale.value = locale;
+  }
+  /**
+   * NOTE:
+   * If you need to specify the language setting for headers, such as the `fetch` API, set it here.
+   * The following is an example for axios.
+   *
+   * axios.defaults.headers.common['Accept-Language'] = locale
+   */
+  document && document.querySelector("html")!.setAttribute("lang", locale);
+  // loadLocaleMessages(i18n, "ro");
+}
+
+export async function loadLocaleMessages(i18n: any, locale = "en") {
+  // load locale messages with dynamic import
+  const messages = await import(
+    /* webpackChunkName: "locale-[request]" */ `./locales/${locale}.json`
+  );
+
+  // set locale and locale message
+  i18n.global.setLocaleMessage(locale, messages.default);
+
+  return nextTick();
+}
+
+export default setupI18n();
